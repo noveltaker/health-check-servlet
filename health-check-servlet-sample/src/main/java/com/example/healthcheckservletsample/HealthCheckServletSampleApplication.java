a3967a02d36602a1fa241dc6f37ce2670698e8e9
gon-zo <pnw123456@icloud.com>
2022-03-17 22:01:45 +0900

    chore: 코드 포멧 변경

---
+++ health-check-servlet-sample/src/main/java/com/example/healthcheckservletsample/HealthCheckServletSampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.healthcheckservletsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCheckServletSampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HealthCheckServletSampleApplication.class, args);
+  }
+}
---
+++ health-check-servlet-sample/src/test/java/com/example/healthcheckservletsample/HealthCheckServletSampleApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.healthcheckservletsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCheckServletSampleApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
